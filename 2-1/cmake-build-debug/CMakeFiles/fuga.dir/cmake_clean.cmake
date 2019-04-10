b8e8b7ab363e7dc35a7bf6adda8e751fde20fb9e
yuu <dptypo@gmail.com>
2019-04-11 02:13:06 +0900

    init

---
+++ 2-1/cmake-build-debug/CMakeFiles/fuga.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuga.dir/dfs.cpp.o"
+  "fuga.pdb"
+  "fuga"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fuga.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2-1/dfs.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yuu on 2019-04-11.
+//
+
+#include <iostream>
+
+int  main(){
+    std::cout<<std::endl<<"Hello CLion"<<std::endl;
+    return 0;
+}
